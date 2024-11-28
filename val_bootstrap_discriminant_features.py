cc0cf3259b717eabd49c160c176255c22c3af651
Claudéric <clauderic.deroy@umontreal.ca>
2024-11-28 12:59:29 -0500

    wrote and runned the validation bootstrap

---
+++ val_bootstrap_discriminant_features.py
@@ -0,0 +1,129 @@
+"""
+script for the validation bootstrap on discriminant features
+"""
+
+import glob
+import numpy as np
+import random
+import pandas as pd
+import os
+from tqdm import tqdm
+from scipy import stats
+
+from nilearn import datasets
+from nilearn.maskers import NiftiLabelsMasker 
+
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score
+from sklearn.pipeline import Pipeline 
+from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.model_selection import permutation_test_score
+
+from sklearn.metrics import confusion_matrix
+
+import joblib
+from pathlib import Path
+
+
+
+
+def machine_learning(x, y):
+    # split the sample o training/test with a 80/20 % ratio 
+    # and stratify sex by class, also shuffle the data
+    X_train, X_test, y_train, y_test = train_test_split( 
+                                                        x,  # x 
+                                                        y,  # y 
+                                                        test_size = 0.2, # 80%/20% split 
+                                                        shuffle = True,  #shuffle dataset before splitting
+                                                        stratify = y,  # keep distribution of sex_class consistent between train and test sets
+                                                        random_state = 123) #same shuffle each time 
+
+    print('train:', len(X_train),'test:', len(X_test))
+    
+    
+    score = []
+    model = LinearSVC()
+    score.append(cross_val_score(model, X_train, y_train, cv=20, n_jobs = 3))
+    print(score)
+    
+    
+    model.fit(X_train,y_train) #fit the model/ train the model
+    y_pred = model.predict(X_test)
+
+    #calculate the model accuracy
+    acc_test = model.score(X_test, y_test)
+    acc_train = model.score(X_train, y_train) 
+
+    #compute the confusion matrix
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    tn, fp, fn, tp = conf_matrix.ravel()
+    df_result = pd.DataFrame(data=[[tp, fn],[fp,tn]], index= ["Femme", "Homme"], columns = ["Femme", "Homme"])
+    df_result = df_result.rename_axis("actual class")
+    df_result = df_result.rename_axis("predicted class", axis="columns")
+    print(df_result)
+    
+    # print results
+    print('accuracy test (r2) =', acc_test)
+    print('accuracy train (r2) =', acc_train)
+    
+    return {
+        "len_X_train": len(X_train),
+        "len_X_test": len(X_test),
+        "score": score,
+        "confusion_matrix": df_result,
+        "acc_test": acc_test,
+        "acc_train": acc_train,
+        "model": model,
+    }
+
+
+
+# the best feature (seed and their region)
+seeds = ["opIFG_L", "opIFG_L", "aMTG_L", "opIFG_L", "aMTG_L", "aMTG_L", "aMTG_L", "planumtemp_L", "aMTG_L", "aMTG_L"]
+regions = ["L_S_front_sup", 
+          "R_G_pariet_inf-Angular", 
+          "L_G_cingul-Post-ventral", 
+          "L_G_and_S_transv_frontopol", 
+          "L_S_intrapariet_and_P_trans", 
+          "R_G_cingul-Post-ventral",
+           "R_S_front_inf",
+           "L_Pole_occipital",
+           "L_G_and_S_cingul-Ant",
+           "R_S_orbital_lateral"]
+
+file_path = "/data/brambati/dataset/HCP/derivatives/seed-to-voxel-nilearn/results_2024/atlas_means/destrieux_fisherz/seed-{seed_name}_atlas-destrieux_mean-all4_gmcorrected__fisherz.csv"
+Final_df = pd.read_csv("/data/brambati/dataset/HCP/derivatives/training_sex_diff/HCP_matched_age_educ_train.csv")
+
+def run(file_path, Final_df, seed, region):
+    x = []
+    y = []
+    file = file_path.format(seed_name=seed)
+    loaded = pd.read_csv(file_path.format(seed_name=seed))
+    signal = loaded[seed+"__"+region]
+    for subject in Final_df["Subject"]:
+        sub_sex = Final_df.loc[Final_df["Subject"] == subject]["Gender"].values[0]
+        value = loaded.loc[loaded["Unnamed: 0"] == "sub-"+str(subject)][seed+"__"+region].values[0]
+        x.append(value)
+        y.append(sub_sex)
+    x = np.asarray(x)
+    x = x.reshape(-1,1)
+    results = machine_learning(x,y)
+
+    return results
+
+
+if __name__ == "__main__":
+    study_dir = "/data/brambati/dataset/HCP/derivatives/training_sex_diff/results_validation_discriminant_features_2024-11-28"
+    file_path = "/data/brambati/dataset/HCP/derivatives/seed-to-voxel-nilearn/results_2024/atlas_means/destrieux_fisherz/seed-{seed_name}_atlas-destrieux_mean-all4_gmcorrected__fisherz.csv"
+    Final_df = pd.read_csv("/data/brambati/dataset/HCP/derivatives/training_sex_diff/HCP_matched_age_educ_train.csv")
+    
+    for i in seeds:
+        iteration_number = 10000
+        results = Parallel(n_jobs=-1, verbose=100)(
+            delayed(run)(file_path, Final_df, seeds[i], regions[i]) for num in range(iteration_number)
+        )
+
+    results_file = study_dir  / f"models_iteration-{iteration_number}.pkl"
+    dump(results, results_file)
